1b6008645159a4b8b8796563a9c1bf78052be04d
Quentin Barrand <quba@redhat.com>
2022-08-05 10:15:05 +0200

    Make gomock work in reflect mode in a vendored environment.

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,3 +1,5 @@
 package client
 
+import _ "github.com/golang/mock/mockgen/model"
+
 //go:generate mockgen -package=client -destination mock_client.go sigs.k8s.io/controller-runtime/pkg/client Client,StatusWriter
