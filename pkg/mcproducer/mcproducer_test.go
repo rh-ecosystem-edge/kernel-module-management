23902e3d707d94285d139365cba2e33f5ec32d1e
Yevgeny Shnaidman <60741237+yevgeny-shnaidman@users.noreply.github.com>
2023-03-20 11:40:45 +0200

    Adding MC CR generation external package for KMM Day1 support (#474)
    This PR add am external package that will allow user to generate an MC CR
    yaml file. When applied , the MCO will cause a kernel module to be replaced
    with a customer's image before NetworkManager service starts running ( basically Day1).
    Pre-requisite: at least one valid network interface should be running on the node without MCO
    interference
    The produced MC CR defines 2 systemd services:
    1) pull service: executes after network interface (NM) is configured, and is responsible for pull
        kernel module image, based on DTK, into the node. Once successfull, it reboots the node
    2) replace kernel module service: executes before NM is configured ( no network). In case the image is already
       present on the node, it will remove the in-tree kernel module, and will replce it with the kernel module
       stored in the DTK-based image
    
    The input to function that produce MC CR yaml is:
    1) MC CR name - the name that will be set into the MC.name
    2) MCP reference - reference to the MachineConfigPool, which will define which nodes that MC CR will affect.
    3) image name - DriverContainer image, that will contain the needed kerne module driver
    4) the name of the kernel driver - which kernel driver to remove/insert

---
+++ pkg/mcproducer/mcproducer_test.go
@@ -0,0 +1,40 @@
+package mcproducer
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"os"
+)
+
+var _ = Describe("ProduceMachineConfig", func() {
+	const (
+		name             = "name"
+		mcpRef           = "mcpRef"
+		kernelModuleName = "testKernelModuleName"
+	)
+
+	It("image name format is invalid", func() {
+		imageName := "quay.io/project/repo@sha256:1f5f1ae25db67aa82707e1b1dc96c8a53ef7094f320b7eeaef12be9a13fa251d"
+
+		res, err := ProduceMachineConfig(name, mcpRef, imageName, kernelModuleName)
+
+		Expect(err).To(HaveOccurred())
+		Expect(res).To(Equal(""))
+
+	})
+
+	// unit performs verification of the MC output vs the manually created
+	// machineconfig-test.yaml. This yaml is created by manually running bas64
+	// encoding on testdata/pull-image-test.sh and testdata/replace-kernel-module-test.sh
+	// and setting the values into the testdata/machineconfig-test.yaml
+	It("verify correct mco output", func() {
+		imageName := "quay.io/project/repo:some-tag12"
+
+		res, err := ProduceMachineConfig(name, mcpRef, imageName, kernelModuleName)
+
+		Expect(err).ToNot(HaveOccurred())
+		expectedRes, err := os.ReadFile("testdata/machineconfig-test.yaml")
+		Expect(err).ToNot(HaveOccurred())
+		Expect(res).To(Equal(string(expectedRes)))
+	})
+})
---
+++ pkg/mcproducer/testdata/pull-image-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if podman image list | grep quay.io/project/repo | grep -q some-tag12; then
+    echo "Image quay.io/project/repo:some-tag12 found in the local registry.Nothing to do"
+else
+    echo "Image quay.io/project/repo:some-tag12 not found in the local registry, pulling"
+    podman pull --authfile /var/lib/kubelet/config.json quay.io/project/repo:some-tag12
+    if [ $? -eq 0 ]; then
+        echo "Image quay.io/project/repo:some-tag12 has been successfully pulled, rebooting.."
+        reboot
+    else
+        echo "Failed to pull image quay.io/project/repo:some-tag12"
+    fi
+fi
