59fca5c9fdaafc09a8a6514210c0db2703244f7a
Quentin Barrand <quentin@quba.fr>
2023-08-01 06:58:59 +0200

    Add the worker application (#678)
    Implement basic features: pull kmod images, extract them on the
    filesystem and load / unload them.
    
    Upstream-Commit: 7be4f34b0c44d7fb262bda137037705c7cd46105

--- go.mod
+++ go.mod
@@ -9,11 +9,13 @@ require (
 	github.com/google/go-cmp v0.5.9
 	github.com/google/go-containerregistry v0.15.2
 	github.com/google/go-containerregistry/pkg/authn/kubernetes v0.0.0-20230523181351-c3f8a49229d3
+	github.com/hashicorp/go-multierror v1.1.1
 	github.com/mitchellh/hashstructure/v2 v2.0.2
 	github.com/onsi/ginkgo/v2 v2.11.0
 	github.com/onsi/gomega v1.27.10
 	github.com/openshift/api v0.0.0-20230425161039-9a7a7c17dbf8
 	github.com/prometheus/client_golang v1.16.0
+	github.com/spf13/cobra v1.7.0
 	gopkg.in/yaml.v3 v3.0.1
 	k8s.io/api v0.27.4
 	k8s.io/apimachinery v0.27.4
@@ -49,7 +51,9 @@ require (
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/pprof v0.0.0-20210720184732-4bb14d4b1be1 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.16.5 // indirect
---
+++ internal/worker/cmdlog.go
@@ -0,0 +1,77 @@
+package worker
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os/exec"
+	"sync"
+
+	"github.com/go-logr/logr"
+	"github.com/hashicorp/go-multierror"
+)
+
+type CommandLogger struct {
+	logger         logr.Logger
+	stdErr, stdOut io.Reader
+	wg             *sync.WaitGroup
+}
+
+func NewCommandLogger(cmd *exec.Cmd, logger logr.Logger) (*CommandLogger, error) {
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return nil, fmt.Errorf("could not obtain a pipe to stderr")
+	}
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, fmt.Errorf("could not obtain a pipe to stdout")
+	}
+
+	cl := CommandLogger{
+		logger: logger,
+		stdErr: stderr,
+		stdOut: stdout,
+		wg:     &sync.WaitGroup{},
+	}
+
+	return &cl, nil
+}
+
+func (cl *CommandLogger) Wait() error {
+	const goroutines = 2
+
+	cl.wg.Add(goroutines)
+	errs := make(chan error, goroutines)
+
+	go cl.write(cl.stdErr, "stderr", errs)
+	go cl.write(cl.stdOut, "stdout", errs)
+
+	cl.wg.Wait()
+	close(errs)
+
+	// TODO move to errors.Join() when we move to Go 1.20
+	var err error
+
+	for chErr := range errs {
+		err = multierror.Append(err, chErr)
+	}
+
+	return err
+}
+
+func (cl *CommandLogger) write(r io.Reader, name string, errs chan<- error) {
+	defer cl.wg.Done()
+
+	logger := cl.logger.WithName(name)
+
+	s := bufio.NewScanner(r)
+
+	for s.Scan() {
+		logger.Info(s.Text())
+	}
+
+	if err := s.Err(); err != nil {
+		errs <- err
+	}
+}
