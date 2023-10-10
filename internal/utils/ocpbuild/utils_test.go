6df45d4a1b08bfcf480006291725dcbe1613f85a
Quentin Barrand <quentin@quba.fr>
2023-10-10 14:38:50 +0200

    Fix support bundle collection (#820)
    Add common labels to worker, build and signing pods so that their logs
    are collected in the support bundle.
    
    Upstream-Commit: 3f05a766241f848076349c7157f8308cf32ed4a6

--- internal/utils/ocpbuild/utils_test.go
+++ internal/utils/ocpbuild/utils_test.go
@@ -4,8 +4,40 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	buildv1 "github.com/openshift/api/build/v1"
+	"github.com/rh-ecosystem-edge/kernel-module-management/internal/api"
+	"github.com/rh-ecosystem-edge/kernel-module-management/internal/constants"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+var _ = Describe("GetOCPBuildLabels", func() {
+	const (
+		kernelVersion = "some-kernel-version"
+		moduleName    = "some-module"
+		buildType     = "some-build-type"
+	)
+
+	It("should work as expected", func() {
+		mld := &api.ModuleLoaderData{
+			KernelVersion: kernelVersion,
+			Name:          moduleName,
+		}
+
+		expected := map[string]string{
+			constants.ModuleNameLabel:     moduleName,
+			constants.BuildTypeLabel:      buildType,
+			constants.TargetKernelTarget:  kernelVersion,
+			"app.kubernetes.io/name":      "kmm",
+			"app.kubernetes.io/component": buildType,
+			"app.kubernetes.io/part-of":   "kmm",
+		}
+
+		Expect(
+			GetOCPBuildLabels(mld, buildType),
+		).To(
+			Equal(expected),
+		)
+	})
+})
 
 var _ = Describe("IsBuildChanged", func() {
 	b0 := &buildv1.Build{
