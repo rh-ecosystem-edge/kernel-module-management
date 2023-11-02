3fe3df4731e32a4d1f7906aeea1b2e008a32afa3
Enrique Belarte Luque <ebelarte@redhat.com>
2023-11-02 14:41:54 +0100

    Add message to different kmm-ci modules (#867)

--- ci/kmm-kmod/kmm_ci_a.c
+++ ci/kmm-kmod/kmm_ci_a.c
@@ -8,12 +8,12 @@ MODULE_DESCRIPTION("A simple kernel module for KMM CI");
 MODULE_VERSION("0.01");
 
 static int __init kmm_ci_init(void) {
-    printk(KERN_INFO "Hello, World!\n");
+    printk(KERN_INFO "Hello, World!. Loaded kmm-ci-a.\n");
     return 0;
 }
 
 static void __exit kmm_ci_exit(void) {
-    printk(KERN_INFO "Goodbye, World!\n");
+    printk(KERN_INFO "Goodbye, World!. Unloaded kmm-ci-a\n");
 }
 
 module_init(kmm_ci_init);
--- ci/kmm-kmod/kmm_ci_b.c
+++ ci/kmm-kmod/kmm_ci_b.c
@@ -1,1 +1,20 @@
-kmm_ci_a.c
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Quentin Barrand");
+MODULE_DESCRIPTION("A simple kernel module for KMM CI");
+MODULE_VERSION("0.01");
+
+static int __init kmm_ci_init(void) {
+    printk(KERN_INFO "Hello, World!. Loaded kmm-ci-b.\n");
+    return 0;
+}
+
+static void __exit kmm_ci_exit(void) {
+    printk(KERN_INFO "Goodbye, World!. Unloaded kmm-ci-b\n");
+}
+
+module_init(kmm_ci_init);
+module_exit(kmm_ci_exit);
