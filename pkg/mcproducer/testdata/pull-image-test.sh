bdb58f8e1018cf501d5533fb7d11ce58873b47e2
Yevgeny Shnaidman <60741237+yevgeny-shnaidman@users.noreply.github.com>
2024-01-23 15:35:40 +0200

    Day1 KMM solution should support V2 with scratch images (#939)
    Currently day1 solution relies on the fact that images
    kernel module images used are DTK-based image, and therefore
    contain modprobe command. Since KMM V2 we now support basic scratch
    images. This PR adds support for scratch images by running worker image
    with supplied config

--- pkg/mcproducer/testdata/pull-image-test.sh
+++ pkg/mcproducer/testdata/pull-image-test.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-if podman image exists quay.io/project/repo:some-tag12; then
-    echo "Image quay.io/project/repo:some-tag12 found in the local registry.Nothing to do"
+
+if [ -e /var/lib/image_file_day1.tar ]; then
+    echo "File /var/lib/image_file_day1.tar found.Nothing to do"
 else
-    echo "Image quay.io/project/repo:some-tag12 not found in the local registry, pulling"
+    echo "File /var/lib/image_file_day1.tar is not on the filesystem, pulling image quay.io/project/repo:some-tag12"
     podman pull --authfile /var/lib/kubelet/config.json quay.io/project/repo:some-tag12
     if [ $? -eq 0 ]; then
-        echo "Image quay.io/project/repo:some-tag12 has been successfully pulled, rebooting.."
-        reboot
+        echo "Image quay.io/project/repo:some-tag12 has been successfully pulled"
     else
         echo "Failed to pull image quay.io/project/repo:some-tag12"
+        exit 1
+    fi
+    echo "Saving image quay.io/project/repo:some-tag12 into a file /var/lib/image_file_day1.tar"
+    podman save -o /var/lib/image_file_day1.tar quay.io/project/repo:some-tag12
+    if [ $? -eq 0 ]; then
+        echo "Image quay.io/project/repo:some-tag12 has been successfully save on file /var/lib/image_file_day1.tar, rebooting..."
+        reboot
+    else
+        echo "Failed to save image quay.io/project/repo:some-tag12 to file /var/lib/image_file_day1.tar"
     fi
 fi
