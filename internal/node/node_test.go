dd1183b52eba87cb10db66c3135a7648500051ab
TomerNewman <tnewman@redhat.com>
2024-07-17 10:35:26 +0300

    Shorten UpdateNodeLabelsAndRecordEvents function in nmc_reconciler.go (#1158)
    Today, the UpdateNodeLabelsAndRecordEvents function has around 100 lines of code. This commit aims to enhance its readability and maintainability by using helper functions, thereby reducing its total length.
    Additionally, adding unit tests that covers the new helper functions.

--- internal/node/node_test.go
+++ internal/node/node_test.go
@@ -3,13 +3,66 @@ package node
 import (
 	"context"
 	"fmt"
-
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/rh-ecosystem-edge/kernel-module-management/internal/client"
+	"github.com/rh-ecosystem-edge/kernel-module-management/internal/utils"
 	"go.uber.org/mock/gomock"
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+var _ = Describe("IsNodeSchedulable", func() {
+	var (
+		ctrl              *gomock.Controller
+		clnt              *client.MockClient
+		mn                Node
+		isNodeSchedulable bool
+	)
+
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+		clnt = client.NewMockClient(ctrl)
+		mn = NewNode(clnt)
+	})
+
+	It("Returns false, node is not schedulable", func() {
+
+		node := v1.Node{
+			Spec: v1.NodeSpec{
+				Taints: []v1.Taint{
+					{
+						Effect: v1.TaintEffectNoSchedule,
+					},
+					{
+						Effect: v1.TaintEffectPreferNoSchedule,
+					},
+				},
+			},
+		}
+		isNodeSchedulable = mn.IsNodeSchedulable(&node)
+		Expect(isNodeSchedulable).To(BeFalse())
+
+	})
+	It("Returns true, node is schedulable", func() {
+
+		node := v1.Node{
+			Spec: v1.NodeSpec{
+				Taints: []v1.Taint{
+					{
+						Effect: v1.TaintEffectNoExecute,
+					},
+					{
+						Effect: v1.TaintEffectPreferNoSchedule,
+					},
+				},
+			},
+		}
+		isNodeSchedulable = mn.IsNodeSchedulable(&node)
+		Expect(isNodeSchedulable).To(BeTrue())
+
+	})
+})
 
 var _ = Describe("GetNodesListBySelector", func() {
 	var (
@@ -64,5 +117,195 @@ var _ = Describe("GetNodesListBySelector", func() {
 		Expect(err).NotTo(HaveOccurred())
 		Expect(nodes).To(Equal([]v1.Node{node2, node3}))
 
+	})
+})
+
+var (
+	loadedKernelModuleReadyNodeLabel   = utils.GetKernelModuleReadyNodeLabel("loaded-ns", "loaded-n")
+	unloadedKernelModuleReadyNodeLabel = utils.GetKernelModuleReadyNodeLabel("unloaded-ns", "unloaded-n")
+)
+
+var _ = Describe("UpdateLabels", func() {
+	var (
+		ctrl *gomock.Controller
+		n    Node
+		ctx  context.Context
+		clnt *client.MockClient
+	)
+
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+		clnt = client.NewMockClient(ctrl)
+		ctx = context.TODO()
+		n = NewNode(clnt)
+	})
+
+	It("Should work as expected", func() {
+		node := v1.Node{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels: map[string]string{},
+			},
+		}
+		loaded := []string{loadedKernelModuleReadyNodeLabel}
+		unloaded := []string{unloadedKernelModuleReadyNodeLabel}
+
+		clnt.EXPECT().Patch(ctx, gomock.Any(), gomock.Any(), gomock.Any()).Return(nil)
+
+		err := n.UpdateLabels(ctx, &node, loaded, unloaded)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(node.Labels).To(HaveKey(loadedKernelModuleReadyNodeLabel))
+	})
+
+	It("Should fail to patch node", func() {
+		node := v1.Node{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels: map[string]string{},
+			},
+		}
+		loaded := []string{loadedKernelModuleReadyNodeLabel}
+		unloaded := []string{unloadedKernelModuleReadyNodeLabel}
+
+		clnt.EXPECT().Patch(ctx, gomock.Any(), gomock.Any(), gomock.Any()).Return(fmt.Errorf("some error"))
+
+		err := n.UpdateLabels(ctx, &node, loaded, unloaded)
+		Expect(err).To(HaveOccurred())
+
+	})
+})
+
+var _ = Describe("GetNumTargetedNodes", func() {
+	var (
+		ctrl *gomock.Controller
+		clnt *client.MockClient
+		mn   Node
+	)
+
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+		clnt = client.NewMockClient(ctrl)
+		mn = NewNode(clnt)
+	})
+
+	It("There are no schedulable nodes", func() {
+		clnt.EXPECT().List(context.Background(), gomock.Any(), gomock.Any()).Return(fmt.Errorf("some error"))
+
+		numOfNodes, err := mn.GetNumTargetedNodes(context.Background(), map[string]string{})
+
+		Expect(err).To(HaveOccurred())
+		Expect(numOfNodes).To(Equal(0))
+	})
+
+	It("Return the number of schedulable nodes only", func() {
+		node1 := v1.Node{
+			Spec: v1.NodeSpec{
+				Taints: []v1.Taint{
+					{
+						Effect: v1.TaintEffectNoSchedule,
+					},
+				},
+			},
+		}
+		node2 := v1.Node{}
+		node3 := v1.Node{
+			Spec: v1.NodeSpec{
+				Taints: []v1.Taint{
+					{
+						Effect: v1.TaintEffectPreferNoSchedule,
+					},
+				},
+			},
+		}
+		clnt.EXPECT().List(context.Background(), gomock.Any(), gomock.Any()).DoAndReturn(
+			func(_ interface{}, list *v1.NodeList, _ ...interface{}) error {
+				list.Items = []v1.Node{node1, node2, node3}
+				return nil
+			},
+		)
+		numOfNodes, err := mn.GetNumTargetedNodes(context.Background(), map[string]string{})
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(numOfNodes).To(Equal(2))
+
+	})
+})
+
+var _ = Describe("FindNodeCondition", func() {
+	var (
+		ctrl           *gomock.Controller
+		clnt           *client.MockClient
+		mn             Node
+		conditionFound *v1.NodeCondition
+	)
+
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+		clnt = client.NewMockClient(ctrl)
+		mn = NewNode(clnt)
+	})
+
+	It("Finds condition", func() {
+		condition := v1.NodeCondition{
+			Type: v1.NodeReady,
+		}
+		node := v1.Node{
+			Status: v1.NodeStatus{
+				Conditions: []v1.NodeCondition{
+					condition,
+				},
+			},
+		}
+		conditionFound = mn.FindNodeCondition(node.Status.Conditions, v1.NodeReady)
+		Expect(conditionFound).To(Equal(&condition))
+	})
+
+	It("Does not find condition", func() {
+		node := v1.Node{
+			Status: v1.NodeStatus{
+				Conditions: []v1.NodeCondition{
+					{
+						Type: v1.NodeDiskPressure,
+					},
+					{
+						Type: v1.NodeMemoryPressure,
+					},
+				},
+			},
+		}
+		conditionFound = mn.FindNodeCondition(node.Status.Conditions, v1.NodeReady)
+		Expect(conditionFound).To(BeNil())
+	})
+})
+
+var _ = Describe("addLabels", func() {
+	var node v1.Node
+
+	BeforeEach(func() {
+		node = v1.Node{}
+	})
+
+	It("Should add labels", func() {
+		labels := []string{loadedKernelModuleReadyNodeLabel}
+		addLabels(&node, labels)
+		Expect(node.Labels).To(HaveKey(loadedKernelModuleReadyNodeLabel))
+	})
+})
+
+var _ = Describe("removeLabels", func() {
+	var node v1.Node
+
+	BeforeEach(func() {
+		node = v1.Node{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels: map[string]string{
+					loadedKernelModuleReadyNodeLabel: "",
+				},
+			},
+		}
+	})
+
+	It("Should remove labels", func() {
+		labels := []string{loadedKernelModuleReadyNodeLabel}
+		removeLabels(&node, labels)
+		Expect(node.Labels).ToNot(HaveKey(loadedKernelModuleReadyNodeLabel))
 	})
 })
