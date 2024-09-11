e203ecc89eba9ff358de4d21a0e8f2ce976d1a02
TomerNewman <tnewman@redhat.com>
2024-09-11 16:32:28 +0300

    Adapt Day1 feature to support init container in worker pod
    Today, KMM has an init container in the worker pod.
    This means that the init container copies all of the .ko files to the shared volume,
    which will be loaded into the kernel by the worker container.
    This commit adds this flow to the Day1 feature.

--- pkg/mcproducer/scripts/pull-image.sh
+++ pkg/mcproducer/scripts/pull-image.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 
-kernel_module_image_filepath="$KERNEL_MODULE_IMAGE_FILEPATH"
 worker_image="$WORKER_IMAGE"
 kernel_module_image="$KERNEL_MODULE_IMAGE"
 kernel_module_image_tag=$(uname -r)
 full_kernel_module_image="$kernel_module_image:$kernel_module_image_tag"
 
-if [ -e $kernel_module_image_filepath ]; then
-    echo "File $kernel_module_image_filepath found. Nothing to do, the file was handled, removing $kernel_module_image_filepath and $kmm_config_file_filepath"
-    rm -f $kernel_module_image_filepath
+if [ -n "$(podman images -q $full_kernel_module_image 2> /dev/null)" ]; then
+    echo "Image $full_kernel_module_image exist locally. Nothing to do, removing $kmm_config_file_filepath"
     rm -f $kmm_config_file_filepath
 else
     podman pull --authfile /var/lib/kubelet/config.json $worker_image
@@ -20,7 +18,7 @@ else
         exit 1
     fi
 
-    echo "File $kernel_module_image_filepath is not on the filesystem, pulling image $full_kernel_module_image"
+    echo "Pulling image $full_kernel_module_image"
     podman pull --authfile /var/lib/kubelet/config.json $full_kernel_module_image
     if [ $? -eq 0 ]; then
         echo "Image $full_kernel_module_image has been successfully pulled"
@@ -28,12 +26,6 @@ else
         echo "Failed to pull image $full_kernel_module_image"
         exit 1
     fi
-    echo "Saving image $full_kernel_module_image into a file $kernel_module_image_filepath"
-    podman save -o $kernel_module_image_filepath $full_kernel_module_image
-    if [ $? -eq 0 ]; then
-        echo "Image $full_kernel_module_image has been successfully save on file $kernel_module_image_filepath, rebooting..."
-        reboot
-    else
-        echo "Failed to save image $full_kernel_module_image to file $kernel_module_image_filepath"
-    fi
+    echo "Rebooting..."
+    reboot
 fi
--- pkg/mcproducer/scripts/replace-kernel-module.sh
+++ pkg/mcproducer/scripts/replace-kernel-module.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 kmm_config_file_filepath="$WORKER_CONFIG_FILEPATH"
-kernel_module_image_filepath="$KERNEL_MODULE_IMAGE_FILEPATH"
 in_tree_module_to_remove="$IN_TREE_MODULE_TO_REMOVE"
 kernel_module="$KERNEL_MODULE"
 worker_image="$WORKER_IMAGE"
+kernel_module_image="$KERNEL_MODULE_IMAGE"
+kernel_module_image_tag=$(uname -r)
+full_kernel_module_image="$kernel_module_image:$kernel_module_image_tag"
+worker_pod_name=kmm-pod
+worker_volume_name=kmm-volume
 
 create_kmm_config() {
     # Write YAML content to the file
     cat <<EOF > "$kmm_config_file_filepath"
-containerImage: $kernel_module_image_filepath
+containerImage: $full_kernel_module_image
 inTreeModuleToRemove: $in_tree_module_to_remove
 modprobe:
   dirName: /opt
@@ -19,17 +23,44 @@ EOF
     cat "$kmm_config_file_filepath"
 }
 
-echo "before checking image tar file presence"
-if [ -e $kernel_module_image_filepath ]; then
-    echo "Image file $kernel_module_image_filepath found on the local file system, creating kmm config file"
+echo "before checking image presence"
+if [ -n "$(podman images -q $full_kernel_module_image 2> /dev/null)" ]; then
+    echo "Image $full_kernel_module_image found on the local file system, creating kmm config file"
     create_kmm_config
-    echo "running kernel-management worker image"
-    podman run --user=root --privileged -v /lib/modules:/lib/modules -v $kmm_config_file_filepath:/etc/kmm-worker/config.yaml -v $kernel_module_image_filepath:$kernel_module_image_filepath $worker_image kmod load --tarball /etc/kmm-worker/config.yaml
+    echo "creating volume"
+    podman volume create $worker_volume_name
+    podman pod create --name $worker_pod_name
+    echo "creating init container"
+    podman create \
+      --pod $worker_pod_name \
+      --init-ctr=always \
+      --rm \
+      -v $worker_volume_name:/tmp \
+      $full_kernel_module_image \
+      /bin/sh -c "mkdir -p /tmp/opt/lib/modules && \
+      cp -R /opt/lib/modules/* /tmp/opt/lib/modules"
+    echo "creating worker container"
+    worker_pod_id=$(
+    podman create \
+      --pod $worker_pod_name\
+      --user=root \
+      --privileged \
+      --rm \
+      -v $worker_volume_name:/tmp \
+      -v /lib/modules:/lib/modules \
+      -v $kmm_config_file_filepath:/etc/kmm-worker/config.yaml \
+      $worker_image \
+      kmod load /etc/kmm-worker/config.yaml)
+    echo "running worker pod"
+    podman pod start $worker_pod_name
     if [ $? -eq 0 ]; then
         echo "OOT kernel module $kernel_module is inserted"
     else
         echo "failed to insert OOT kernel module $kernel_module"
     fi
+    podman wait $worker_pod_id
+    echo "removing volume"
+    podman volume rm $worker_volume_name
 else
-    echo "Image file $kernel_module_image_filepath is not present in local registry, will try after reboot"
+    echo "Image $full_kernel_module_image is not present in local registry, will try after reboot"
 fi
