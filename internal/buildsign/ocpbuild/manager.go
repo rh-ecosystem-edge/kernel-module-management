23914f4d8f02d8d52004f5583ff708db2cfa92a1
Yevgeny Shnaidman <yshnaidm@redhat.com>
2025-04-29 15:23:15 +0300

    Adding manager and maker/signer to buildsign package
    With the introduction of the MBSC controller, the flow is changing and
    there is no need for 2 different managers. There is no need to decide if
    the build/sign should be executed, since that decision is taken by the MIC controller.
    The new package must provide only the following capabilities:
    1. Get the running OCP Builds (build or sign), in order for MBSC controller to
       update its status
    2. Sync the current build/sign OCP Builds based on action parameter
    3. Delete build/sign successfull OCP Builds based on the action parameter
    
    The following files are added:
    1. manager - it will decide, based on the action field, which action
       should be taken: check OCP Build, create OCP Build etc'. It will use a dedicated
    maker/signer interfaces when creating/deleting relevant pod. In addition
    it will return the status of the running build/sign OCP Builds
    2. maker - will be responsible for creating the "Build" OCP Build. Copied almost
       as is from the current build package
    3. signer - will be responsible for creating the "Sign" OCP Build. Copied almost
       as is from the current sign package

---
+++ internal/buildsign/ocpbuild/manager.go
@@ -0,0 +1,157 @@
+package ocpbuild
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	buildv1 "github.com/openshift/api/build/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	kmmv1beta1 "github.com/rh-ecosystem-edge/kernel-module-management/api/v1beta1"
+	"github.com/rh-ecosystem-edge/kernel-module-management/internal/api"
+	"github.com/rh-ecosystem-edge/kernel-module-management/internal/buildsign"
+	"github.com/rh-ecosystem-edge/kernel-module-management/internal/kernel"
+	"github.com/rh-ecosystem-edge/kernel-module-management/internal/syncronizedmap"
+	"github.com/rh-ecosystem-edge/kernel-module-management/internal/utils"
+)
+
+type manager struct {
+	client          client.Client
+	maker           maker
+	signer          signer
+	ocpbuildManager ocpbuildManager
+}
+
+func NewManager(client client.Client,
+	helper buildsign.Helper,
+	kernelOsDtkMapping syncronizedmap.KernelOsDtkMapping,
+	signImage string,
+	scheme *runtime.Scheme) buildsign.Manager {
+	ocpbuildManager := newOCPBuildManager(client)
+	maker := newMaker(client, helper, ocpbuildManager, kernelOsDtkMapping, scheme)
+	signer := newSigner(client, signImage, ocpbuildManager, scheme)
+	return &manager{
+		client:          client,
+		maker:           maker,
+		signer:          signer,
+		ocpbuildManager: ocpbuildManager,
+	}
+}
+
+func (m *manager) GetStatus(ctx context.Context, name, namespace, kernelVersion string,
+	action kmmv1beta1.BuildOrSignAction, owner metav1.Object) (kmmv1beta1.BuildOrSignStatus, error) {
+	ocpbuildType := ocpbuildTypeBuild
+	if action == kmmv1beta1.SignImage {
+		ocpbuildType = ocpbuildTypeSign
+	}
+	normalizedKernel := kernel.NormalizeVersion(kernelVersion)
+	foundOCPBuild, err := m.ocpbuildManager.getModuleOCPBuildByKernel(ctx, name, namespace, normalizedKernel, ocpbuildType, owner)
+	if err != nil {
+		if !errors.Is(err, ErrNoMatchingBuild) {
+			return kmmv1beta1.BuildOrSignStatus(""), fmt.Errorf("failed to get ocpbuild %s/%s, action %s: %v", namespace, name, action, err)
+		}
+		return kmmv1beta1.BuildOrSignStatus(""), nil
+	}
+	status, err := m.ocpbuildManager.getOCPBuildStatus(foundOCPBuild)
+	if err != nil {
+		return kmmv1beta1.BuildOrSignStatus(""), fmt.Errorf("failed to get status from the ocpbuild %s/%s, action %s: %v",
+			foundOCPBuild.Namespace, foundOCPBuild.Name, action, err)
+	}
+	switch status {
+	case StatusCompleted:
+		return kmmv1beta1.ActionSuccess, nil
+	case StatusFailed:
+		return kmmv1beta1.ActionFailure, nil
+	}
+
+	// any other status means the pod is still not finished, returning empty status
+	return kmmv1beta1.BuildOrSignStatus(""), nil
+}
+
+func (m *manager) Sync(ctx context.Context, mld *api.ModuleLoaderData, pushImage bool, action kmmv1beta1.BuildOrSignAction, owner metav1.Object) error {
+	logger := log.FromContext(ctx)
+	var (
+		ocpbuildType     string
+		ocpbuildTemplate *buildv1.Build
+		err              error
+	)
+	switch action {
+	case kmmv1beta1.BuildImage:
+		logger.Info("Building in-cluster")
+		ocpbuildType = ocpbuildTypeBuild
+		ocpbuildTemplate, err = m.maker.makeBuildTemplate(ctx, mld, pushImage, owner)
+	case kmmv1beta1.SignImage:
+		logger.Info("Signing in-cluster")
+		ocpbuildType = ocpbuildTypeSign
+		ocpbuildTemplate, err = m.signer.makeBuildTemplate(ctx, mld, pushImage, owner)
+	default:
+		return fmt.Errorf("invalid action %s", action)
+	}
+
+	if err != nil {
+		return fmt.Errorf("could not make Pod template: %v", err)
+	}
+
+	b, err := m.ocpbuildManager.getModuleOCPBuildByKernel(ctx, mld.Name, mld.Namespace,
+		mld.KernelNormalizedVersion, ocpbuildType, owner)
+
+	if err != nil {
+		if !errors.Is(err, ErrNoMatchingBuild) {
+			return fmt.Errorf("error getting the %s ocpbuild: %v", ocpbuildType, err)
+		}
+
+		logger.Info("Creating build")
+		err = m.ocpbuildManager.createOCPBuild(ctx, ocpbuildTemplate)
+		if err != nil {
+			return fmt.Errorf("could not create Build: %v", err)
+		}
+
+		return nil
+	}
+
+	changed, err := m.ocpbuildManager.isOCPBuildChanged(b, ocpbuildTemplate)
+	if err != nil {
+		return fmt.Errorf("could not determine if ocpbuild has changed: %v", err)
+	}
+
+	if changed {
+		logger.Info("The module's spec has been changed, deleting the current ocpbuild so a new one can be created", "name", b.Name, "action", action)
+		err = m.ocpbuildManager.deleteOCPBuild(ctx, b)
+		if err != nil {
+			logger.Info(utils.WarnString(fmt.Sprintf("failed to delete %s ocpbuild %s: %v", ocpbuildType, b.Name, err)))
+		}
+	}
+
+	return nil
+}
+
+func (m *manager) GarbageCollect(ctx context.Context, name, namespace string, action kmmv1beta1.BuildOrSignAction, owner metav1.Object) ([]string, error) {
+	ocpbuildType := ocpbuildTypeBuild
+	if action == kmmv1beta1.SignImage {
+		ocpbuildType = ocpbuildTypeSign
+	}
+	builds, err := m.ocpbuildManager.getModuleOCPBuilds(ctx, name, namespace, ocpbuildType, owner)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get %s buils for mbsc %s/%s: %v", ocpbuildType, namespace, name, err)
+	}
+
+	logger := log.FromContext(ctx)
+	errs := make([]error, 0, len(builds))
+	deleteBuildsNames := make([]string, 0, len(builds))
+	for _, build := range builds {
+		if build.Status.Phase == buildv1.BuildPhaseComplete {
+			err = m.ocpbuildManager.deleteOCPBuild(ctx, &build)
+			errs = append(errs, err)
+			if err != nil {
+				logger.Info(utils.WarnString("failed to delete %s build %s in garbage collection: %v"), ocpbuildType, build.Name, err)
+				continue
+			}
+			deleteBuildsNames = append(deleteBuildsNames, build.Name)
+		}
+	}
+	return deleteBuildsNames, errors.Join(errs...)
+}
---
+++ internal/buildsign/ocpbuild/manager_test.go
@@ -0,0 +1,332 @@
+package ocpbuild
+
+import (
+	"context"
+	"fmt"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	buildv1 "github.com/openshift/api/build/v1"
+	"go.uber.org/mock/gomock"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	kmmv1beta1 "github.com/rh-ecosystem-edge/kernel-module-management/api/v1beta1"
+	"github.com/rh-ecosystem-edge/kernel-module-management/internal/api"
+	"github.com/rh-ecosystem-edge/kernel-module-management/internal/client"
+	"github.com/rh-ecosystem-edge/kernel-module-management/internal/kernel"
+)
+
+var _ = Describe("GetStatus", func() {
+	var (
+		ctrl                *gomock.Controller
+		clnt                *client.MockClient
+		mockMaker           *Mockmaker
+		mockSigner          *Mocksigner
+		mockOCPBuildManager *MockocpbuildManager
+		mgr                 *manager
+	)
+	const (
+		mbscName      = "some-name"
+		mbscNamespace = "some-namespace"
+		kernelVersion = "some version"
+	)
+
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+		clnt = client.NewMockClient(ctrl)
+		mockMaker = NewMockmaker(ctrl)
+		mockSigner = NewMocksigner(ctrl)
+		mockOCPBuildManager = NewMockocpbuildManager(ctrl)
+		mgr = &manager{
+			client:          clnt,
+			maker:           mockMaker,
+			signer:          mockSigner,
+			ocpbuildManager: mockOCPBuildManager,
+		}
+	})
+
+	ctx := context.Background()
+	testMBSC := kmmv1beta1.ModuleBuildSignConfig{}
+
+	It("failed flow, getModuleOCPBuildByKernel fails", func() {
+		normalizedKernel := kernel.NormalizeVersion(kernelVersion)
+		mockOCPBuildManager.EXPECT().getModuleOCPBuildByKernel(ctx, mbscName, mbscNamespace, normalizedKernel, ocpbuildTypeBuild, &testMBSC).
+			Return(nil, fmt.Errorf("some error"))
+
+		status, err := mgr.GetStatus(ctx, mbscName, mbscNamespace, kernelVersion, kmmv1beta1.BuildImage, &testMBSC)
+		Expect(err).To(HaveOccurred())
+		Expect(status).To(Equal(kmmv1beta1.BuildOrSignStatus("")))
+	})
+
+	It("getModuleOCPBuildByKernel returns pod does not exists", func() {
+		normalizedKernel := kernel.NormalizeVersion(kernelVersion)
+		mockOCPBuildManager.EXPECT().getModuleOCPBuildByKernel(ctx, mbscName, mbscNamespace, normalizedKernel, ocpbuildTypeBuild, &testMBSC).
+			Return(nil, ErrNoMatchingBuild)
+
+		status, err := mgr.GetStatus(ctx, mbscName, mbscNamespace, kernelVersion, kmmv1beta1.BuildImage, &testMBSC)
+		Expect(err).To(BeNil())
+		Expect(status).To(Equal(kmmv1beta1.BuildOrSignStatus("")))
+	})
+
+	It("failed flow, getOCPBuildStatus fails", func() {
+		foundBuild := buildv1.Build{}
+		normalizedKernel := kernel.NormalizeVersion(kernelVersion)
+		gomock.InOrder(
+			mockOCPBuildManager.EXPECT().getModuleOCPBuildByKernel(ctx, mbscName, mbscNamespace, normalizedKernel, ocpbuildTypeBuild, &testMBSC).
+				Return(&foundBuild, nil),
+			mockOCPBuildManager.EXPECT().getOCPBuildStatus(&foundBuild).Return(Status(""), fmt.Errorf("some error")),
+		)
+
+		status, err := mgr.GetStatus(ctx, mbscName, mbscNamespace, kernelVersion, kmmv1beta1.BuildImage, &testMBSC)
+		Expect(err).To(HaveOccurred())
+		Expect(status).To(Equal(kmmv1beta1.BuildOrSignStatus("")))
+	})
+
+	DescribeTable("check good flow and returned statuses", func(buildStatus Status, expectedStatus kmmv1beta1.BuildOrSignStatus) {
+		foundBuild := buildv1.Build{}
+		normalizedKernel := kernel.NormalizeVersion(kernelVersion)
+		gomock.InOrder(
+			mockOCPBuildManager.EXPECT().getModuleOCPBuildByKernel(ctx, mbscName, mbscNamespace, normalizedKernel, ocpbuildTypeBuild, &testMBSC).
+				Return(&foundBuild, nil),
+			mockOCPBuildManager.EXPECT().getOCPBuildStatus(&foundBuild).Return(buildStatus, nil),
+		)
+		status, err := mgr.GetStatus(ctx, mbscName, mbscNamespace, kernelVersion, kmmv1beta1.BuildImage, &testMBSC)
+		Expect(err).To(BeNil())
+		Expect(status).To(Equal(expectedStatus))
+	},
+		Entry("build's status is success", StatusCompleted, kmmv1beta1.ActionSuccess),
+		Entry("build's status is failure", StatusFailed, kmmv1beta1.ActionFailure),
+		Entry("build's status is in progress", StatusInProgress, kmmv1beta1.BuildOrSignStatus("")),
+		Entry("build's status is in unknown", Status(""), kmmv1beta1.BuildOrSignStatus("")),
+	)
+})
+
+var _ = Describe("Sync", func() {
+	var (
+		ctrl                *gomock.Controller
+		clnt                *client.MockClient
+		mockMaker           *Mockmaker
+		mockSigner          *Mocksigner
+		mockOCPBuildManager *MockocpbuildManager
+		mgr                 *manager
+	)
+	const (
+		mbscName      = "some-name"
+		mbscNamespace = "some-namespace"
+		kernelVersion = "some version"
+	)
+
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+		clnt = client.NewMockClient(ctrl)
+		mockMaker = NewMockmaker(ctrl)
+		mockSigner = NewMocksigner(ctrl)
+		mockOCPBuildManager = NewMockocpbuildManager(ctrl)
+		mgr = &manager{
+			client:          clnt,
+			maker:           mockMaker,
+			signer:          mockSigner,
+			ocpbuildManager: mockOCPBuildManager,
+		}
+	})
+
+	ctx := context.Background()
+	testMBSC := kmmv1beta1.ModuleBuildSignConfig{}
+	testMLD := &api.ModuleLoaderData{
+		Name:                    mbscName,
+		Namespace:               mbscNamespace,
+		KernelNormalizedVersion: kernelVersion,
+	}
+
+	It("makeBuildTemplate failed", func() {
+		By("test build action")
+		mockMaker.EXPECT().makeBuildTemplate(ctx, testMLD, true, &testMBSC).Return(nil, fmt.Errorf("some error"))
+		err := mgr.Sync(ctx, testMLD, true, kmmv1beta1.BuildImage, &testMBSC)
+		Expect(err).To(HaveOccurred())
+
+		By("test sign action")
+		mockSigner.EXPECT().makeBuildTemplate(ctx, testMLD, true, &testMBSC).Return(nil, fmt.Errorf("some error"))
+		err = mgr.Sync(ctx, testMLD, true, kmmv1beta1.SignImage, &testMBSC)
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("GetModulePodByKernel failed", func() {
+		gomock.InOrder(
+			mockMaker.EXPECT().makeBuildTemplate(ctx, testMLD, true, &testMBSC).Return(nil, nil),
+			mockOCPBuildManager.EXPECT().getModuleOCPBuildByKernel(ctx, mbscName, mbscNamespace, kernelVersion, ocpbuildTypeBuild, &testMBSC).
+				Return(nil, fmt.Errorf("some error")),
+		)
+		err := mgr.Sync(ctx, testMLD, true, kmmv1beta1.BuildImage, &testMBSC)
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("CreateOCPBuild failed", func() {
+		testTemplate := buildv1.Build{}
+		gomock.InOrder(
+			mockMaker.EXPECT().makeBuildTemplate(ctx, testMLD, true, &testMBSC).Return(&testTemplate, nil),
+			mockOCPBuildManager.EXPECT().getModuleOCPBuildByKernel(ctx, mbscName, mbscNamespace, kernelVersion, ocpbuildTypeBuild, &testMBSC).
+				Return(nil, ErrNoMatchingBuild),
+			mockOCPBuildManager.EXPECT().createOCPBuild(ctx, &testTemplate).Return(fmt.Errorf("some error")),
+		)
+		err := mgr.Sync(ctx, testMLD, true, kmmv1beta1.BuildImage, &testMBSC)
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("isOCPBuildChanged failed", func() {
+		testTemplate := buildv1.Build{}
+		testBuild := buildv1.Build{}
+		gomock.InOrder(
+			mockMaker.EXPECT().makeBuildTemplate(ctx, testMLD, true, &testMBSC).Return(&testTemplate, nil),
+			mockOCPBuildManager.EXPECT().getModuleOCPBuildByKernel(ctx, mbscName, mbscNamespace, kernelVersion, ocpbuildTypeBuild, &testMBSC).
+				Return(&testBuild, nil),
+			mockOCPBuildManager.EXPECT().isOCPBuildChanged(&testBuild, &testTemplate).Return(false, fmt.Errorf("some error")),
+		)
+		err := mgr.Sync(ctx, testMLD, true, kmmv1beta1.BuildImage, &testMBSC)
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("deleteOCPBuild failed should not cause failure", func() {
+		testTemplate := buildv1.Build{}
+		testBuild := buildv1.Build{}
+		gomock.InOrder(
+			mockMaker.EXPECT().makeBuildTemplate(ctx, testMLD, true, &testMBSC).Return(&testTemplate, nil),
+			mockOCPBuildManager.EXPECT().getModuleOCPBuildByKernel(ctx, mbscName, mbscNamespace, kernelVersion, ocpbuildTypeBuild, &testMBSC).
+				Return(&testBuild, nil),
+			mockOCPBuildManager.EXPECT().isOCPBuildChanged(&testBuild, &testTemplate).Return(true, nil),
+			mockOCPBuildManager.EXPECT().deleteOCPBuild(ctx, &testBuild).Return(fmt.Errorf("some error")),
+		)
+		err := mgr.Sync(ctx, testMLD, true, kmmv1beta1.BuildImage, &testMBSC)
+		Expect(err).To(BeNil())
+	})
+
+	DescribeTable("check good flow", func(buildAction, buildExists, buildChanged, pushImage bool) {
+		testAction := kmmv1beta1.BuildImage
+		testBuildTemplate := buildv1.Build{}
+		existingTestBuild := buildv1.Build{}
+		buildType := ocpbuildTypeBuild
+		if !buildAction {
+			buildType = ocpbuildTypeSign
+			testAction = kmmv1beta1.SignImage
+		}
+
+		if buildAction {
+			mockMaker.EXPECT().makeBuildTemplate(ctx, testMLD, pushImage, &testMBSC).Return(&testBuildTemplate, nil)
+		} else {
+			mockSigner.EXPECT().makeBuildTemplate(ctx, testMLD, pushImage, &testMBSC).Return(&testBuildTemplate, nil)
+		}
+		var getBuildError error
+		if !buildExists {
+			getBuildError = ErrNoMatchingBuild
+		}
+		mockOCPBuildManager.EXPECT().getModuleOCPBuildByKernel(ctx, mbscName, mbscNamespace, kernelVersion, buildType, &testMBSC).
+			Return(&existingTestBuild, getBuildError)
+		if !buildExists {
+			mockOCPBuildManager.EXPECT().createOCPBuild(ctx, &testBuildTemplate).Return(nil)
+			goto executeTestFunction
+		}
+		mockOCPBuildManager.EXPECT().isOCPBuildChanged(&existingTestBuild, &testBuildTemplate).Return(buildChanged, nil)
+		if buildChanged {
+			mockOCPBuildManager.EXPECT().deleteOCPBuild(ctx, &existingTestBuild).Return(nil)
+		}
+
+	executeTestFunction:
+		err := mgr.Sync(ctx, testMLD, pushImage, testAction, &testMBSC)
+		Expect(err).To(BeNil())
+	},
+		Entry("action build, build OCPBuild does not exists", true, false, false, true),
+		Entry("action sign, sign OCPBuild does not exists", false, false, false, false),
+		Entry("action build, build OCPBuild exists, build object has not changed", true, true, false, true),
+		Entry("action sign, sign OCPBuild exists, build object has not changed", false, true, false, false),
+		Entry("action build, build OCPBuild exists, build object has changed", true, true, true, false),
+		Entry("action sign, sign OCPBuild exists, build object has changed", false, true, true, false),
+	)
+})
+
+var _ = Describe("GarbageCollect", func() {
+	var (
+		ctrl                *gomock.Controller
+		clnt                *client.MockClient
+		mockMaker           *Mockmaker
+		mockSigner          *Mocksigner
+		mockOCPBuildManager *MockocpbuildManager
+		mgr                 *manager
+	)
+	const (
+		mbscName      = "some-name"
+		mbscNamespace = "some-namespace"
+		kernelVersion = "some version"
+	)
+
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+		clnt = client.NewMockClient(ctrl)
+		mockMaker = NewMockmaker(ctrl)
+		mockSigner = NewMocksigner(ctrl)
+		mockOCPBuildManager = NewMockocpbuildManager(ctrl)
+		mgr = &manager{
+			client:          clnt,
+			maker:           mockMaker,
+			signer:          mockSigner,
+			ocpbuildManager: mockOCPBuildManager,
+		}
+	})
+
+	ctx := context.Background()
+	testMBSC := kmmv1beta1.ModuleBuildSignConfig{}
+
+	It("failed to get module buildss", func() {
+		mockOCPBuildManager.EXPECT().getModuleOCPBuilds(ctx, mbscName, mbscNamespace, ocpbuildTypeBuild, &testMBSC).Return(nil, fmt.Errorf("some error"))
+
+		_, err := mgr.GarbageCollect(ctx, mbscName, mbscNamespace, ocpbuildTypeBuild, &testMBSC)
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("delete ocpbuild failed", func() {
+		testBuild := buildv1.Build{
+			Status: buildv1.BuildStatus{
+				Phase: buildv1.BuildPhaseComplete,
+			},
+		}
+		gomock.InOrder(
+			mockOCPBuildManager.EXPECT().getModuleOCPBuilds(ctx, mbscName, mbscNamespace, ocpbuildTypeBuild, &testMBSC).
+				Return([]buildv1.Build{testBuild}, nil),
+			mockOCPBuildManager.EXPECT().deleteOCPBuild(ctx, &testBuild).Return(fmt.Errorf("some error")),
+		)
+
+		_, err := mgr.GarbageCollect(ctx, mbscName, mbscNamespace, ocpbuildTypeBuild, &testMBSC)
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("good flow", func() {
+		testBuildSuccess := buildv1.Build{
+			ObjectMeta: metav1.ObjectMeta{Name: "buildSuccess"},
+			Status:     buildv1.BuildStatus{Phase: buildv1.BuildPhaseComplete},
+		}
+		testBuildFailure := buildv1.Build{
+			ObjectMeta: metav1.ObjectMeta{Name: "buildFailure"},
+			Status:     buildv1.BuildStatus{Phase: buildv1.BuildPhaseFailed},
+		}
+		testBuildRunning := buildv1.Build{
+			ObjectMeta: metav1.ObjectMeta{Name: "buildRunning"},
+			Status:     buildv1.BuildStatus{Phase: buildv1.BuildPhaseRunning},
+		}
+		testBuildError := buildv1.Build{
+			ObjectMeta: metav1.ObjectMeta{Name: "buildError"},
+			Status:     buildv1.BuildStatus{Phase: buildv1.BuildPhaseError},
+		}
+		testBuildPending := buildv1.Build{
+			ObjectMeta: metav1.ObjectMeta{Name: "buildPending"},
+			Status:     buildv1.BuildStatus{Phase: buildv1.BuildPhasePending},
+		}
+		returnedBuilds := []buildv1.Build{testBuildSuccess, testBuildFailure, testBuildRunning, testBuildError, testBuildPending}
+		gomock.InOrder(
+			mockOCPBuildManager.EXPECT().getModuleOCPBuilds(ctx, mbscName, mbscNamespace, ocpbuildTypeBuild, &testMBSC).
+				Return(returnedBuilds, nil),
+			mockOCPBuildManager.EXPECT().deleteOCPBuild(ctx, &testBuildSuccess).Return(nil),
+		)
+
+		res, err := mgr.GarbageCollect(ctx, mbscName, mbscNamespace, ocpbuildTypeBuild, &testMBSC)
+		Expect(err).To(BeNil())
+		Expect(res).To(Equal([]string{"buildSuccess"}))
+	})
+})
