05dbb9b6124a38287ead2cee9205ab52ff7a1a54
Anthony Byrne <abyrne@redhat.com>
2026-08-05 11:10:30 -0400

    Support configurable SecurityContext for DevicePlugin containers
    Add an optional SecurityContext field to DevicePluginContainerSpec. When set,
    it overrides the default privileged security context for DevicePlugin
    containers. When nil, the existing privileged: true default is
    preserved.
    
    Note: upstream also adds this for DRA containers, but 2.6 does not
    support DRA, so that part was dropped from this cherry-pick.
    
    Signed-off-by: Anthony Byrne <abyrne@redhat.com>
    
    Upstream-Commit: c4e4fdc2b934b00917833e2cfeccb5ee2e6e5d51

--- internal/controllers/device_plugin_reconciler.go
+++ internal/controllers/device_plugin_reconciler.go
@@ -470,6 +470,12 @@ func generatePodContainerSpec(
 	if containerSpec == nil {
 		return nil
 	}
+
+	securityContext := containerSpec.SecurityContext
+	if securityContext == nil {
+		securityContext = &v1.SecurityContext{Privileged: ptr.To(true)}
+	}
+
 	return []v1.Container{
 		{
 			Args:            containerSpec.Args,
@@ -479,7 +485,7 @@ func generatePodContainerSpec(
 			Image:           containerSpec.Image,
 			ImagePullPolicy: containerSpec.ImagePullPolicy,
 			Resources:       containerSpec.Resources,
-			SecurityContext: &v1.SecurityContext{Privileged: ptr.To(true)},
+			SecurityContext: securityContext,
 			VolumeMounts:    append(containerSpec.VolumeMounts, presetVolumeMounts...),
 			LivenessProbe:   livenessProbe,
 			StartupProbe:    startupProbe,
